b38f9fd810a52e12f4a24ef3adf22a8ce3a8651d
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 23:38:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
       resp.write item.nil? ? "Item not found" : item.price
+      resp.status = 404
     else
       resp.write "Route not found"
       resp.status = 404
