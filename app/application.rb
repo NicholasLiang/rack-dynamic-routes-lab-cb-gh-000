29eccdb8672388f2d53d53418c4f46909e0f54e7
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 23:33:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
       resp.write item.price
-      
+    else
     end
     
     resp.finish
