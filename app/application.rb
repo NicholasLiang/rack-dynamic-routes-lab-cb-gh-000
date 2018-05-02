76a3415e790a42bb6f8f9e8f35b18650e95b6ee4
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 23:38:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
       
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
+      if 
       resp.write item.nil? ? "Item not found" : item.price
       resp.status = 400
     else
